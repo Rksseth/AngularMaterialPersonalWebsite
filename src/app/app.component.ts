4fdeb62d4896ee745356bb935fdaa562e7d82143
rksseth <rksseth@edu.uwaterloo.ca>
2020-11-11 11:26:55 -0500

    added some cool icons and added cool digital pixel fade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,42 @@ export class AppComponent {
   faGithub = faGithub;
   faEnvelope = faEnvelope;
 
+  matrix = [];
+
   constructor() {
+  }
+
+  ngOnInit(): void {
+    let numRows = 20;
+    let numCols = 40;
+    for (let i = 0; i < numRows; i++) {
+      let weighting1 = numRows - i;
+      let elems = [1,0];
+      let weights = [weighting1, numRows-weighting1];
+
+      let row = [];
+
+      for (let j = 0; j < numCols; j++) {
+        row.push(this.generateWeightedRandomValue(elems, weights));
+      }
+      this.matrix.push(row);
+    }
+    console.log(this.matrix);
+  }
+
+  add(a, b) { return a + b; }
+
+  generateWeightedRandomValue(elems: Array<number>, weights: Array<number>): void {
+    let totalWeight = weights.reduce(this.add, 0);
+
+    let weighedElems = [];
+    let currentElem = 0;
+    while (currentElem < elems.length) {
+      for (let i = 0; i < weights[currentElem]; i++)
+        weighedElems[weighedElems.length] = elems[currentElem];
+      currentElem++;
+    }
+    let rnd = Math.floor(Math.random() * totalWeight);
+    return weighedElems[rnd];
   }
 }
